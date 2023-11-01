8bb3125584510fdf71200dfacb7f5cb79ce21f73
Dilancifci <s233597@dtu.dk>
2023-11-01 11:26:44 +0100

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ##
 golem::fill_desc(
   pkg_name = "centralD", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
+  pkg_title = "centralDogma", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  author_first_name = "Dilan", # Your First Name
+  author_last_name = "Cifci", # Your Last Name
+  author_email = "s233597@dtu.dk", # Your Email
   repo_url = NULL, # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
@@ -37,12 +37,12 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("s233597") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "s233597")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
